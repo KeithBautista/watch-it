29d5f1fe0ed626d76b3ec20c4f905c6df15e2a34
KeithBautista <keith1829@gmail.com>
2022-11-17 14:03:45 +0000

    created new post space in admin page

--- watchit/models.py
+++ watchit/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    """This will delete all of users' posts"""
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+        """This will allow us to see title and author on admin page"""
