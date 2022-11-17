6ac24f4956d6f4311315cabf9183ba4e452b30a5
KeithBautista <keith1829@gmail.com>
2022-11-17 14:14:15 +0000

    added in new view to show all posts

--- watchit/views.py
+++ watchit/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+"""List View does a query set and provides all views of a specific post while
+the detail view shows the specific details of a post"""
 
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#   return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    """This view will show all the posts since we passed in
+    listview"""
