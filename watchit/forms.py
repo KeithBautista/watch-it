7abacdf86f3207a8c44435ba7c2583e0745b591a
KeithBautista <keith1829@gmail.com>
2022-11-22 20:19:39 +0000

    added in JS to get user id for form

--- watchit/forms.py
+++ watchit/forms.py
@@ -19,7 +19,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'postFormAuthor', 'type': 'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             }
