ee2bb17a0b00cfc60513efc21bb842ce2e65e8dc
KeithBautista <keith1829@gmail.com>
2022-11-22 23:41:28 +0000

    added category into update post class in forms

--- watchit/forms.py
+++ watchit/forms.py
@@ -29,10 +29,11 @@ class PostForm(forms.ModelForm):
 class UpdatePost(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body')
+        fields = ('title', 'title_tag', 'category', 'body')
         # Contains the fields that you have in your form in your models.py
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             }
