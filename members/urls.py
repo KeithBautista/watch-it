a5353ff05eae9056890108e2b72eee73d91c5f78
KeithBautista <keith1829@gmail.com>
2022-11-23 23:49:11 +0000

    imported auth views and added change password html

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import UserRegistrationView, UserEditView
+from django.contrib.auth import views as auth_views
+# Allows us to use authentication views from django
 
 urlpatterns = [
     path('registration/', UserRegistrationView.as_view(), name="registration"),
     path('edit_profile/', UserEditView.as_view(), name="edit_profile"),
+    path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
 ]
