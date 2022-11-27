e6b88dadaba7abdbc360491f00d74b1eca8c7b54
KeithBautista <keith1829@gmail.com>
2022-11-27 19:45:48 +0000

    added 404 page and config

--- watch_it/settings.py
+++ watch_it/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8rdmi#v@2b$$2m0v&#qm(-joekkqw=99rk40jc24n)cmp7vbm6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
