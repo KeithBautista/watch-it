e6703549d9fdbadda5875edc7eb1f13087a999b2
KeithBautista <keith1829@gmail.com>
2022-11-28 07:08:02 +0000

    secret key removed django insecure

--- watch_it/settings.py
+++ watch_it/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8rdmi#v@2b$$2m0v&#qm(-joekkqw=99rk40jc24n)cmp7vbm6')
+SECRET_KEY = os.environ.get('SECRET_KEY', '8rdmi#v@2b$$2m0v&#qm(-joekkqw=99rk40jc24n)cmp7vbm6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
