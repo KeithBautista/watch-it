8c6a62802ca37e3e88cab52cdc33bf0a199c5886
KeithBautista <keith1829@gmail.com>
2022-11-28 06:48:23 +0000

    set up environment variables

--- watch_it/settings.py
+++ watch_it/settings.py
@@ -19,6 +19,8 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,13 +29,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8rdmi#v@2b$$2m0v&#qm(-joekkqw=99rk40jc24n)cmp7vbm6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8rdmi#v@2b$$2m0v&#qm(-joekkqw=99rk40jc24n)cmp7vbm6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = ['kb-watchit.herokuapp.com']
-
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
@@ -82,17 +86,17 @@ WSGI_APPLICATION = 'watch_it.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if developement:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
